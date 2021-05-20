c55044dfeab92a3e8bb8230dc769730fdd033c8a
root <972036700@qq.com>
2021-05-20 21:43:26 +0800

    Initial commit

---
+++ feign/src/main/java/com/zg/feign/impl/FeignError.java
@@ -0,0 +1,19 @@
+package com.zg.feign.impl;
+
+import com.zg.feign.FeignProviderClient;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class FeignError implements FeignProviderClient {
+    @Override
+    public Collection<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public String index() {
+        return "服务器维护中......";
+    }
+}
